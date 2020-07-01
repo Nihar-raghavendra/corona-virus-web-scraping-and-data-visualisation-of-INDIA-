66e420b76a18d51c4a76018b3097bfa380f8f99d
Nihar-raghavendra <67226037+Nihar-raghavendra@users.noreply.github.com>
2020-07-01 13:08:22 +0530

    Update corona2.py
    state wise data visualisation of corona visrus cases and deaths.you can select which pie plot you want by user input

--- corona2.py
+++ corona2.py
@@ -1,8 +1,6 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-import random
 from bs4 import BeautifulSoup
 res = requests.get("https://www.deccanherald.com/national/coronavirus-india-update-state-wise-total-number-of-confirmed-cases-deaths-on-june-30-855264.html")
 soup = BeautifulSoup(res.text,"html.parser")
@@ -26,10 +24,39 @@ states.remove('Cases being reassigned to states')
 tc = [int(i) for i in total_cases]
 d = [int(i) for i in deaths]
 column = ["States", 'Covid Cases','Deaths']
-df = pd.DataFrame(list(zip(states,tc,d)),columns=column).sort_values(by=['Covid Cases'])
-plt.figure(figsize=(13,7))
-patches, texts = plt.pie(df['Deaths'],startangle=90, radius=1.3,shadow=True,)
-percent = 100*df['Deaths']/df['Deaths'].sum()
-labels = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(df['States'], percent)]
-plt.legend(patches,labels[::-1],bbox_to_anchor=(-0.1, 1.05))
-plt.show()
+df = pd.DataFrame(list(zip(states,tc,d)),columns=column).sort_values(by=['Deaths','Covid Cases'])
+# piechart deaths wise
+def pie_deaths():
+    plt.figure(figsize=(13,7))
+    plt.title("Deaths")
+    patches, texts = plt.pie(df['Deaths'],startangle=90, radius=1.2,shadow=True,)
+    percent = 100*df['Deaths']/df['Deaths'].sum()
+    labels = [f'{i} - {j:1.2f} %' for i,j in zip(df['States'], percent)]
+    plt.legend(patches,labels[::-1],bbox_to_anchor=(-0.1, 1.05),title="State wise deaths percentage")
+# piechart cases wise
+def pie_cases():
+    plt.figure(figsize=(13,7))
+    plt.title("Covid Cases")
+    patches, texts = plt.pie(df['Covid Cases'],startangle=90, radius=1.2,shadow=True,)
+    percent = 100*df['Covid Cases']/df['Covid Cases'].sum()
+    labels = [f'{i} - {j:1.2f} %' for i,j in zip(df['States'], percent)] #
+    plt.legend(patches,labels[::-1],bbox_to_anchor=(-0.1, 1.05),title="State wise covid cases percentage",fontsize='medium')
+
+
+print("1 Death wise pie plot")
+print("2 Cases wise pie plot")
+print("3 show me two plots")
+try:
+    m = int(input("Enter your choice: "))
+    if m == 1:
+        pie_deaths()
+        plt.show()
+    elif m == 2:
+        pie_cases()
+        plt.show()
+    else:
+        pie_cases()
+        pie_deaths()
+        plt.show()
+except:
+    print("Enter only numbers")
