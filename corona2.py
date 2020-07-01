f23e195c9c84e3e8d94435cc7ceeb3eb4464d6f4
Nihar-raghavendra <67226037+Nihar-raghavendra@users.noreply.github.com>
2020-07-02 00:46:07 +0530

    Add files via upload
    Added choropleth map

--- corona2.py
+++ corona2.py
@@ -1,21 +1,20 @@
 import requests
 import pandas as pd
+import folium
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 res = requests.get("https://www.deccanherald.com/national/coronavirus-india-update-state-wise-total-number-of-confirmed-cases-deaths-on-june-30-855264.html")
 soup = BeautifulSoup(res.text,"html.parser")
 if soup("tbody"):
-
     t_body = soup.find("table")
 tr = t_body.find_all("tr")
-
 states = []
 deaths = []
 total_cases = []
 for i in tr:
     td = i.find_all("td")
     if td:
-        states.append(td[0].text.replace('\xa0'," "))
+        states.append(td[0].text.replace('\xa0'," ").replace('Maharashtra ','Maharashtra'))
         deaths.append(td[2].text.replace(",",""))
         total_cases.append(td[1].text.replace(",","").replace(". ",""))
 total_cases.remove("7285")
@@ -42,10 +41,25 @@ def pie_cases():
     labels = [f'{i} - {j:1.2f} %' for i,j in zip(df['States'], percent)] #
     plt.legend(patches,labels[::-1],bbox_to_anchor=(-0.1, 1.05),title="State wise covid cases percentage",fontsize='medium')
 
+def coro_map():
+    world_map = folium.Map(tiles="Mapbox Bright", location=[20.5937, 78.9629], zoom_start=4.4)
+    geo = r'./yy.geojson'
+    world_map.choropleth(
+        geo_data = geo,
+        data = df,
+        columns =['States','Covid Cases'],
+        fill_color='Set1',
+        key_on = 'feature.properties.NAME_1',
+        legend_name = "Highest Covid-19 recovered patients Countries"
+    )
+    world_map.save('./Map.html')
+    print('Map saved at current location as Map.html')
 
 print("1 Death wise pie plot")
 print("2 Cases wise pie plot")
 print("3 show me two plots")
+print("4 Choropleth Map")
+
 try:
     m = int(input("Enter your choice: "))
     if m == 1:
@@ -54,9 +68,12 @@ try:
     elif m == 2:
         pie_cases()
         plt.show()
-    else:
+    elif m == 3:
         pie_cases()
         pie_deaths()
         plt.show()
+    elif m == 4:
+        coro_map()
+
 except:
     print("Enter only numbers")
